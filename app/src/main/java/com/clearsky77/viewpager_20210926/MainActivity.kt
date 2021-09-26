d3811c3d7f11f2b8e9b8d3157ca67a9fc5bafee2
clearsky77 <clearsky3264@gmail.com>
2021-09-26 13:44:38 +0900

    뷰페이저 어댑터 생성 및 그 위에 연결

--- app/src/main/java/com/clearsky77/viewpager_20210926/MainActivity.kt
+++ app/src/main/java/com/clearsky77/viewpager_20210926/MainActivity.kt
@@ -2,10 +2,18 @@ package com.clearsky77.viewpager_20210926
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.clearsky77.viewpager_20210926.adapters.MainViewPagerAdapter
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var mvpa : MainViewPagerAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        mvpa = MainViewPagerAdapter( supportFragmentManager ) //어댑터를 만들어 주시오.
+        mainViewPager.adapter = mvpa // 메인 페이지에 어댑터를 올려준다.
     }
 }
